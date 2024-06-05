4982d2b250b35d5de29d56a6cadf104f142c9c09
datrine <trinitietp@gmail.com>
2024-06-05 12:13:18 +0100

    docs

--- jobs/job_test.go
+++ jobs/job_test.go
@@ -2,12 +2,8 @@ package jobs
 
 import (
 	"testing"
-
-	"github.com/adjust/rmq/v5"
 )
 
 func TestConnection(t *testing.T) {
-	testConn := rmq.NewTestConnection()
-
-	
+	//testConn := rmq.NewTestConnection()
 }
